0ca793ec31040fa8e575f57a214b7cd95c40a8cd
arshahluwalia <54185054+arshahluwalia@users.noreply.github.com>
2023-07-15 22:12:48 -0400

    making website responsive

--- my-app/src/ParsedResults.jsx
+++ my-app/src/ParsedResults.jsx
@@ -2,31 +2,27 @@ import React from 'react';
 
 export const ParsedResults = ({ showComponent }) => {
   return (
-    <div>
-        {showComponent && 
-        <div className="parsed-results">
-            <div className="bounding-box">
-                <div className="title">
-                    Here are your parsed results!
-                </div>
-                <div>
-                    a bunch of skills. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-                <div>
-                    education. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-                <div>
-                    past work experience. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    a lot of it. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-                <div>
-                    projects. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-                <div>
-                    achievements. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </div>
-            </div>
-        </div>}
+    <div className='bounding-box'>
+    <div className={`${showComponent ? 'parsed-results' : 'hidden'}`}>
+        <div className="title">
+            Here are your parsed results!
+        </div>
+        <div>
+            a bunch of skills. Lorem ipsum dolor sit amet.
+        </div>
+        <div>
+            education. Lorem ipsum dolor sit amet.
+        </div>
+        <div>
+            past work experience. Lorem ipsum dolor sit amet.
+        </div>
+        <div>
+            projects. Lorem ipsum dolor sit amet.
+        </div>
+        <div>
+            achievements. Lorem ipsum dolor sit amet.
+        </div>
+    </div>
     </div>
   );
 };
