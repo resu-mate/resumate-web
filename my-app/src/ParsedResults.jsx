c6fd80f00f07cf78664ced91429e39d9393230a1
arshahluwalia <54185054+arshahluwalia@users.noreply.github.com>
2023-07-15 23:52:21 -0400

    making responsive for mobile phone

--- my-app/src/ParsedResults.jsx
+++ my-app/src/ParsedResults.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 export const ParsedResults = ({ showComponent }) => {
   return (
-    <div className='bounding-box'>
-    <div className={`${showComponent ? 'parsed-results' : 'hidden'}`}>
-        <div className="title">
-            Here are your parsed results!
+    <div className={`${showComponent ? 'bounding-box' : 'hidden'}`}>
+        <div className="parsed-results">
+            <div className="title">
+                Here are your parsed results!
+            </div>
+            <div>
+                a bunch of skills. Lorem ipsum dolor sit amet.
+            </div>
+            <div>
+                education. Lorem ipsum dolor sit amet.
+            </div>
+            <div>
+                past work experience. Lorem ipsum dolor sit amet.
+            </div>
+            <div>
+                projects. Lorem ipsum dolor sit amet.
+            </div>
+            <div>
+                achievements. Lorem ipsum dolor sit amet.
+            </div>
         </div>
-        <div>
-            a bunch of skills. Lorem ipsum dolor sit amet.
-        </div>
-        <div>
-            education. Lorem ipsum dolor sit amet.
-        </div>
-        <div>
-            past work experience. Lorem ipsum dolor sit amet.
-        </div>
-        <div>
-            projects. Lorem ipsum dolor sit amet.
-        </div>
-        <div>
-            achievements. Lorem ipsum dolor sit amet.
-        </div>
-    </div>
     </div>
   );
 };
