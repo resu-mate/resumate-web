8d53f88df2c3b2723bcfd351d54f6375325a9030
arshahluwalia <54185054+arshahluwalia@users.noreply.github.com>
2023-07-19 22:29:13 -0400

    adding back pdf preview & style changes?

--- my-app/src/FileUploader.jsx
+++ my-app/src/FileUploader.jsx
@@ -5,8 +5,10 @@ import { LoadingAnimation } from './LoadingAnimation';
 export const FileUploader = ({ setShowParsedResults, setParsedResults }) => {
   const fileInputRef = useRef(null);
   const [dragOver, setDragOver] = useState(false);
-  const [file, setFile] = useState('');
+  const [fileBinary, setFileBinary] = useState('');
+  const [fileData, setFileData] = useState('');
   const [fileName, setFileName] = useState('');
+  const [fileType, setFileType] = useState('');
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -32,52 +34,59 @@ export const FileUploader = ({ setShowParsedResults, setParsedResults }) => {
     setSubmitted(false);
     const fileName = fileRef.name;
     setFileName(fileName);
-    const reader = new FileReader();
-    // reader.readAsDataURL(fileRef);
-    // reader.onload = (ev) => {
-    //   setFile(ev.target.result);
-    // };
-    reader.readAsBinaryString(fileRef)
-    reader.onload=(ev) => {
-      setFile(`${btoa(ev.target.result)}`)
+    const fileType = fileRef.type;
+    setFileType(fileType);
+
+    const reader1 = new FileReader();
+    reader1.readAsBinaryString(fileRef)
+    reader1.onload=(ev) => {
+      setFileBinary(`${btoa(ev.target.result)}`)
     }
+
+    const reader2 = new FileReader();
+    reader2.readAsDataURL(fileRef);
+    reader2.onload = (ev) => {
+      setFileData(ev.target.result);
+    };
   };
 
   const onChange = (e) => {
     setSubmitted(false);
+    setParsedResults('');
     if ((e.target.files).length > 0) {
       const fileRef = e.target.files[0];
       handleFile(fileRef);
     } else {
-      setFile('');
+      setFileBinary('');
       setFileName('');
     }
   };
 
   const onSubmit = async (e) => {
-    console.log({ file }); // show in console until handled
+    console.log({ fileBinary }); 
     e.preventDefault();
     setSubmitted(true);
     setLoading(true); 
+    setParsedResults('');
 
     try {
-      if (!file) {
+      if (!fileBinary) {
         console.error('no file submitted');
         setLoading(false); 
         setShowParsedResults(true);
         return;
       }
 
-      // if (file.indexOf('application/pdf') <= -1) {
-      //   console.error('not a pdf');
-      //   setLoading(false); 
-      //   setShowParsedResults(true);
-      //   return;
-      // }
+      if (fileType != "application/pdf") {
+        console.error('not a pdf');
+        setLoading(false); 
+        setShowParsedResults(true);
+        return;
+      }
 
       const response = await fetch('https://3trpak7uyg.execute-api.us-east-1.amazonaws.com/dev/upload-file', {
         method: 'POST',
-        body: file,
+        body: fileBinary,
         headers: {
           'Content-Type': 'application/pdf',
         },
@@ -104,11 +113,12 @@ export const FileUploader = ({ setShowParsedResults, setParsedResults }) => {
     e.preventDefault();
     setDragOver(false);
     fileInputRef.current.value = '';
-    setFile('');
+    setFileBinary('');
     setFileName('');
     setSubmitted(false);
     setLoading(false);
     setShowParsedResults(false); 
+    setParsedResults('')
   };
 
 
@@ -124,23 +134,24 @@ export const FileUploader = ({ setShowParsedResults, setParsedResults }) => {
           <div className="file-chooser">
             <input ref={fileInputRef} type="file" accept=".pdf, .doc, .docx" onChange={onChange}/>
           </div>
-        </div>
-        {/* <div>
-            {submitted && file && file.indexOf('application/pdf') > -1 && 
+
+            {fileBinary && fileType == 'application/pdf' && 
             <div className="preview-container">
-              <embed src={file} height="400px" type="application/pdf"/>
+              <embed src={`${fileData}#toolbar=0&navpanes=0`} height="100%" type="application/pdf"/>
+              {/* <iframe src={`${fileData}#toolbar=0`} height="100%" width="100%" title="Embedded PDF" /> */}
             </div>}
 
-            {submitted && file && file.indexOf('application/msword') > -1 && (
+            {/* { fileBinary && file.indexOf('application/msword') > -1 && (
             <div className="preview-container">
               <p>TODO: This is a .doc file preview</p>
             </div>)}
 
-            {submitted && file && file.indexOf('application/vnd.openxmlformats-officedocument.wordprocessingml.document') > -1 && (
+            {fileBinary && file.indexOf('application/vnd.openxmlformats-officedocument.wordprocessingml.document') > -1 && (
             <div className="preview-container">
               <p>TODO: This is a .docx file preview</p>
-            </div>)}
-        </div> */}
+            </div>)} */}
+
+        </div>
         <div>
           <button className="button" onClick={handleReset}>
               Reset
@@ -150,7 +161,7 @@ export const FileUploader = ({ setShowParsedResults, setParsedResults }) => {
             </button>
         </div>
         <div>
-            {submitted && file && 
+            {submitted && fileBinary && 
             <div className="body-text">
               You submitted {fileName}!
             </div>}
